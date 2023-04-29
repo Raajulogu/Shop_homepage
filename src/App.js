d1b8e712db83ce789029a3eb86a6cf1748b7fc5c
Raajulogu <rajeshkumarlogu145@gmail.com>
2023-04-30 00:27:49 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -2,74 +2,74 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import React,{useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 function App() {
   let data=[{item:"Fancy Product",
-        rating:"0",
+        rating:"",
         price:"",
         price1:"$40.00-$80.00",
         sale:""},
         {item:"Special Item",
-        rating:"5",
+        rating:"⭐⭐⭐⭐⭐",
         price:"$20.00 ",
         price1:"$18.00",
         sale:"sale"},
         {item:"Sale Item",
-        rating:"0",
+        rating:"",
         price:"$50.00 ",
         price1:"$25.00",
         sale:"sale"},
         {item:"Popular Item",
-        rating:"5",
+        rating:"⭐⭐⭐⭐⭐",
         price:"",
         price1:"$40.00",
         sale:""},
         {item:"Sale Item",
-        rating:"0",
+        rating:"",
         price:"$50.00 ",
         price1:"$25.00",
         sale:"sale"},
         {item:"Fancy Product",
-        rating:"0",
+        rating:"",
         price:"",
         price1:"$120.00-$280.00",
         sale:""},
         {item:"Special Item",
-        rating:"5",
+        rating:"⭐⭐⭐⭐⭐",
         price:"$20.00 ",
         price1:"$18.00",
         sale:"sale"},
         {item:"Popular Item",
-        rating:"5",
+        rating:"⭐⭐⭐⭐⭐",
         price:"",
-        price1:"$40.00"}
+        price1:"$40.00",
+        sale:""}
       ]
       let [count,setcount]=useState(0)
       let [option,setoption]=useState(false)
   return (
     <div className="App">
       <div className="head">
-        <button>Start Bootstrap</button>
-        <button>Home</button>
-        <button>About</button>
-        <span className="option"><button onClick={()=>{
-          setoption(!option)
-        }}>Shop
-        <p>All Products</p>
-        <p>Popular Items</p>
-        <p>New Arrivals</p></button>
-        {option ?
-        <div className='list'>
-        <p>All Products</p>
-        <p>Popular Items</p>
-        <p>New Arrivals</p>
-        </div> : ""}</span>
-        
-        <span className='cart'>Cart {count}</span>
+        <button className="head_option_title">Start Bootstrap</button>
+        <button className="head_option">Home</button>
+        <button className="head_option">About</button>
+        <span className="option">
+          <button className="head_option_shop" onClick={()=>{setoption(!option)}}>Shop ▼
+          </button>
+          {option ?
+          <div className='list'>
+          <p className='list_option_head'>All Products</p>
+          <p className='list_option'>Popular Items</p>
+          <p className='list_option'>New Arrivals</p>
+          </div> : ""}
+          </span>
+        <span className='cart'>🛒Cart {count}</span>
       </div>
       <div className='title'>
-        <h1>Shop in style</h1>
-        <p>With this shop homepage template</p>
+        <h1 className='title_head'>Shop in style</h1>
+        <p className='title_content'>With this shop homepage template</p>
       </div>
 
       <div className="card-container">
@@ -101,12 +101,15 @@ function Product({prod,count,setcount}){
   return(
     <div>
       <Card style={{ width: '18rem' }} className='Card'>
-      <div className='img'><div className='sale'>
-        <p id="sale">{prod.sale}</p>
-        </div>450X300</div>
+      <div className='img'>
+        <div className='sale'>
+        {prod.sale!=="" ?
+        <p id="sale">{prod.sale}</p>:""
+        }
+        </div><div className='img_content'>450X300</div></div>
       <Card.Body>
-        <Card.Title>{prod.item}</Card.Title>
-        <Card.Title>{prod.rating}</Card.Title>
+        <Card.Title className='item_name'>{prod.item}</Card.Title>
+        <Card.Title className='rating'>{prod.rating}</Card.Title>
         <Card.Title><span id="old_price">{prod.price}</span>{prod.price1}</Card.Title>
         {cart ? <Button id="primary" variant="primary"
         onClick={addtocart}
